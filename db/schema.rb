c4f89314a5c00b54d2a8e287a76d8ee59cb75986
ltviet100 <ltviet100@gmail.com>
2019-02-18 23:41:12 +0700

    Create users table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_155518) do
+ActiveRecord::Schema.define(version: 2019_02_18_163824) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2019_02_18_155518) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
