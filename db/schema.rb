036f271fd30838d9b6d86332f4b757f3cea139f1
ltviet100 <ltviet100@gmail.com>
2019-02-19 00:17:25 +0700

    Create association between user and article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_163824) do
+ActiveRecord::Schema.define(version: 2019_02_18_170331) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "ckeditor_assets", force: :cascade do |t|
