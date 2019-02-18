b726432d7fc424e6441ee93ca321c63b9a7c59a1
ltviet100 <ltviet100@gmail.com>
2019-02-19 00:31:53 +0700

    Add user on article and add delegate method in model article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 100 }
   validates :description, presence: true, length: { minimum: 5 }
   belongs_to :user
+  delegate :username, to: :user, allow_nil: true
 end
