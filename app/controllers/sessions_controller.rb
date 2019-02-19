b65be26d7f6a31edc9411fc8b0e5f3939818c2a1
ltviet100 <ltviet100@gmail.com>
2019-02-19 17:40:36 +0700

    Complete create, destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have logged in successfully!"
+      redirect_to user_path(user)
+    else
+      flash[:danger] = "Failed login!"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logout successfully!"
+    redirects_to root_path
   end
 end
