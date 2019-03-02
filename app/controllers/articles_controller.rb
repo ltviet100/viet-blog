7cd5ff8510a781d071e5fb122989ea74a7d248ac
ltviet100 <ltviet100@gmail.com>
2019-03-02 14:19:00 +0700

    Final project

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,66 +1,62 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :show, :update, :destroy]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   before_action :require_user, except: [:index, :show]
-  before_action :require_exact_user, only: [:edit, :update, :destroy]
-  def index
-    if params[:search]
-      @articles = Article.search(params[:search]).order("created_at DESC")
-    else
-      @articles = Article.paginate(page: params[:page], per_page: 5)
-    end
-  end
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
-  def show
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
     @article = Article.new
   end
 
+  def edit
+  end
+
   def create
     @article = Article.new(article_params)
-    @article.user= current_user
+    @article.user = current_user
     if @article.save
-      flash[:success] = "Article was created!"
+      flash[:success] = "Article was created successfully"
       redirect_to article_path(@article)
     else
-      flash[:info] = "Article was not created!"
       render 'new'
     end
   end
 
-  def edit
-  end
-
   def update
     if @article.update(article_params)
-      flash[:success] = "Article was updated!"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
-      flash[:info] = "Article was not updated!"
       render 'edit'
     end
   end
 
+  def show
+  end
+
   def destroy
     @article.destroy
-    flash[:danger] = "Article was removed!"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :description, :user_id, category_ids: [])
-    end
 
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    def require_exact_user
-      if current_user != @article.user && !current_user.admin?
-        flash[:danger] = "You can only edit your articles!"
-        redirect_to articles_path
-      end
+  def article_params
+    params.require(:article).permit(:title, :description, :user_id, category_ids: [])
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to root_path
     end
+  end
 end
