c669f395abcdadc8d8e700135d6f5fd0658ebc67
ltviet100 <ltviet100@gmail.com>
2019-03-02 16:45:57 +0700

    Fxi error when user not log in click category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
   def logged_in?
     !!current_user
   end
